c402edd808250a3b03548e956df78275b0d0264f
hachispin <ganalonrainier255@gmail.com>
2025-08-03 01:15:25 +0100

    fixed chapter selection parsing

--- python/mdex_tool/cli/utils.py
+++ python/mdex_tool/cli/utils.py
@@ -44,7 +44,7 @@ class CliUtils:
         print(option)
         return option
 
-    def parse_chapter_selection(
+    def parse_chapter_selection(  # pylint:disable=too-many-return-statements
         self, user_input: str, error_out: Callable[[str], None]
     ) -> list[int]:
         """
@@ -67,23 +67,49 @@ class CliUtils:
         """
         user_input = user_input.strip(",")  # Remove trailing
         selections = tuple(s.strip() for s in user_input.split(","))
-        nums = []
+        nums = []  # type: list[int]
 
-        if not selections:
+        if all(not s for s in selections):
             error_out("Selection can't be blank")
             return []
 
-        if any(ch not in "0123456789-" for s in selections for ch in s):
-            ...
+        allowed = set("0123456789-")
+        if any(ch not in allowed for s in selections for ch in s):
+            error_out(
+                "Unexpected character; only numbers, dashes, commas and spaces are allowed"
+            )
+            return []
 
         for s in selections:
+            # all chars in `s` must now be digits or dashes
             if not s:
-                error_out("No selection found between comma (e.g. '4,,2')")
+                error_out("No selection found between comma (e.g. 2,,4)")
                 return []
+
             if s.isdigit():
                 nums.append(int(s))
+                continue
 
-        return []
+            # must include one or more dashes if not digit
+            if s.count("-") > 1:
+                error_out(f"{repr(s)}: Range selections can only have one dash")
+                return []
+
+            start, _, stop = s.partition("-")
+            if not start or not stop:
+                error_out(
+                    f"{repr(s)}: Both sides of a dash must have numbers to be a valid range"
+                )
+                return []
+
+            if int(start) > int(stop):
+                error_out(
+                    f"{repr(s)}: A range selection's start must be below its end."
+                )
+                return []
+            nums += range(int(start), int(stop) + 1)
+
+        return sorted(list(set(nums)))
 
     def print_manga_titles(self, manga: tuple[Manga, ...]):
         """Prints manga titles with a left index for use by the user."""
