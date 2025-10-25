b42d9eb06d41970f54b721e7fdfa1786332e2971
hachispin <ganalonrainier255@gmail.com>
2025-10-25 21:03:49 +0100

    refactor to conform to `pylint` and `pyright` standards

--- python/notepad/main.py
+++ python/notepad/main.py
@@ -1,10 +1,12 @@
+"""The entry point, which also contains the PyQt6 GUI code."""
+
 import sys
 from pathlib import Path
 
-from PyQt6.QtCore import Qt, QSize
-from PyQt6.QtGui import QBrush
-from PyQt6.QtWidgets import (
-    QApplication,
+from PyQt6.QtCore import Qt, QSize  # pylint: disable=no-name-in-module
+from PyQt6.QtGui import QBrush  # pylint: disable=no-name-in-module
+from PyQt6.QtWidgets import (  # pylint: disable=no-name-in-module
+    QApplication,  # pylint: disable=no-name-in-module
     QMainWindow,
     QPushButton,
     QLineEdit,
@@ -17,8 +19,9 @@ from PyQt6.QtWidgets import (
     QSizePolicy,
 )
 
-from NotepadSystem import Note, NoteCollection
-from NotepadSystem import (
+from notepad.notepad_system import (
+    Note,
+    NoteCollection,
     NoteException,
     TitleAlreadyExistsError,
     BlankTitleError,
@@ -36,26 +39,29 @@ if not APP_DIR.exists():
     raise FileNotFoundError("Could not find parent dir")
 
 if not NOTES_DATA.exists():
-    NOTES_DATA.write_text("")
+    NOTES_DATA.write_text("", encoding="utf-8")
 
 
 class MainWindow(QMainWindow):
+    """Main window for notepad."""
+
     def __init__(self):
         super().__init__()
         self.setWindowTitle("notepad")  # Should consider a better name soon
         self.setMinimumSize(QSize(640, 360))
 
-        self.collection = self.load_data()
+        self.collection: NoteCollection = self.load_data()
         menubar = QHBoxLayout()
         top = QVBoxLayout()
+
         # Menubar
-        self.curr_note_title = QLineEdit()
+        self.curr_note_title: QLineEdit = QLineEdit()
         self.curr_note_title.setPlaceholderText("New title")
         self.curr_note_title.setMaxLength(24)
         self.curr_note_title.setFixedWidth(280)
-        self.dropdown = QComboBox()
-        self.save = QPushButton("Save")
-        self.delete = QPushButton("Delete")
+        self.dropdown: QComboBox = QComboBox()
+        self.save: QPushButton = QPushButton("Save")
+        self.delete: QPushButton = QPushButton("Delete")
 
         least_width = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
 
@@ -66,12 +72,12 @@ class MainWindow(QMainWindow):
         menubar.addWidget(self.save)
         menubar.addWidget(self.delete)
         self.update_dropdown()
-        # Body
-        # Not set until note picked
-        self.curr_note_body = QTextEdit()
+
+        # Body, not set until note picked
+        self.curr_note_body: QTextEdit = QTextEdit()
         self.curr_note_body.setPlaceholderText("Start writing a new note here...")
-        # Mappings
-        # Note that dropdown index - 1 = NoteCollection index
+
+        # Mappings, note that (dropdown_index - 1) = NoteCollection_index
         self.dropdown.currentIndexChanged.connect(self.display_note)
         self.save.clicked.connect(self.save_curr_note)
         self.delete.clicked.connect(self.delete_curr_note)
@@ -82,19 +88,29 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(container)
         container.setLayout(top)
 
-    def handle_note_exception(self, error: Exception, *, fatal=False):
-        # Please remember to use return after calling this
-        if fatal:
-            print("Fatal error:", error)
-            QMessageBox.critical(
-                self,
-                "Fatal error",
-                "Something went wrong and the program must exit! "
-                "Please raise an issue if you see this message. "
-                "(or if you're the dev, do your job!)",
-            )
-            sys.exit(1)
+    def handle_exception(self, error: Exception):
+        """
+        Generic exception handler.
 
+        If `error` is not a `NoteException` instance, a message
+        box is displayed and `sys.exit()` is called afterwards.
+        """
+        if isinstance(error, NoteException):
+            self.handle_note_exception(error)
+
+        error_type = type(error).__name__
+        error_help = str(error)
+
+        QMessageBox.critical(
+            self,
+            f"Unexpected {error_type}",
+            f"{error_help}\n\nPress any button to exit...",
+        )
+
+        sys.exit(1)
+
+    def handle_note_exception(self, error: NoteException):
+        """Provides a message box detailing the `error`, which is a `NoteException`."""
         match error:
             case TitleAlreadyExistsError():
                 QMessageBox.warning(self, "Save failed", "This title is already taken.")
@@ -112,17 +128,19 @@ class MainWindow(QMainWindow):
                 QMessageBox.warning(
                     self, "Invalid import", "Imported notes format could not be read"
                 )
-            case _:
-                # logger is for losers
-                print("Non critical error (?):", error)
+            case _:  # should be unreachable
+                raise error
 
     def load_data(self) -> NoteCollection:
+        """Parses `NOTES_DATA` and returns a `NoteCollection`."""
+
         return NoteCollection.from_json(NOTES_DATA)
 
     def save_notes(self):
+        """Writes the currently stored `NoteCollection` to `NOTES_DATA`."""
         NoteCollection.write_to_json(self.collection, NOTES_DATA)
 
-    def get_note_by_dropdown(self, dd_idx=None) -> Note:
+    def get_note_by_dropdown(self, dd_idx: int | None = None) -> Note:
         """Returns the Note object at current or chosen dropdown index"""
 
         if dd_idx is None:
@@ -130,7 +148,7 @@ class MainWindow(QMainWindow):
 
         elif dd_idx == 0:
             e = ValueError("Attempted to access reserved index 0 (New note...)")
-            self.handle_note_exception(e, fatal=True)
+            self.handle_exception(e)
 
         try:
             return self.collection.notes[dd_idx - 1]
@@ -140,8 +158,7 @@ class MainWindow(QMainWindow):
             info = f"num_notes={num_notes}, num_dd_items={num_dd_items}"
             e = IndexError(f"Index ({dd_idx} - 1) out of notes ({info})")
 
-            self.handle_note_exception(e, fatal=True)
-            assert False  # Unreachable, for type checkers
+            self.handle_exception(e)
 
     def update_dropdown(self):
         """Updates dropdown items with stored notes"""
@@ -150,8 +167,8 @@ class MainWindow(QMainWindow):
         # Set "New note..." to gray for UX
         dd.clear()
         dd.addItem("New note...")
-        idx = dd.model().index(0, 0)  # type: ignore[union-attr]
-        dd.model().setData(  # type: ignore[union-attr]
+        idx = dd.model().index(0, 0)  # pyright: ignore[reportOptionalMemberAccess]
+        dd.model().setData(  # pyright: ignore[reportOptionalMemberAccess]
             idx, QBrush(Qt.GlobalColor.gray), Qt.ItemDataRole.ForegroundRole
         )
 
@@ -175,14 +192,20 @@ class MainWindow(QMainWindow):
             title_ui.setText("")
             body_ui.setPlainText("")
 
-    def save_curr_note(self):  # Also does editing
+    def save_curr_note(self):
+        """
+        Saves the current note being written by the user.
+
+        If "saving" is invoked on an already-existing
+        note, its equivalent to an "edit" of that note.
+        """
         saved_title = self.curr_note_title.text().strip()
         saved_body = self.curr_note_body.toPlainText().strip()
         curr = self.dropdown.currentIndex()
 
         try:
             if curr == 0:  # Add
-                new = Note.new_note(saved_title, saved_body)
+                new = Note.new(saved_title, saved_body)
                 self.collection.add_note(new)
             else:  # Edit
                 curr_note = self.get_note_by_dropdown(curr)
@@ -199,6 +222,12 @@ class MainWindow(QMainWindow):
         return
 
     def delete_curr_note(self):
+        """
+        Deletes the current note being displayed to the user.
+
+        If the "note" hasn't been saved yet, the body and title are cleared.
+        """
+
         title = self.curr_note_title
         body = self.curr_note_body
 
--- python/notepad/notepad_system.py
+++ python/notepad/notepad_system.py
@@ -1,7 +1,12 @@
+"""Contains the `Note` class and other note-related classes."""
+
 import json
 from pathlib import Path
+import sys
+from textwrap import dedent
 import time
 from datetime import datetime
+from typing import override
 
 
 # Note related exceptions
@@ -15,8 +20,8 @@ class TitleAlreadyExistsError(NoteException):
     note within a `NoteCollection` instance
     """
 
-    def __init__(self, title):
-        self.title = title
+    def __init__(self, title: str):
+        self.title: str = title
         super().__init__(f"Note with title '{title}' already exists")
 
 
@@ -41,8 +46,8 @@ class NotFoundError(NoteException):
     of a `NoteCollection` instance
     """
 
-    def __init__(self, title):
-        self.title = title
+    def __init__(self, title: str):
+        self.title: str = title
         super().__init__(f"Note with title '{title}' not found")
 
 
@@ -56,27 +61,43 @@ class FormatError(NoteException):
 
 
 class Note:
-    def __init__(self, title, body, created_at, last_edited):
+    """
+    Contains the fields:
+
+    - `title` (str)
+    - `body` (str)
+    - `created_at` (str) [property]
+    - `last_edited` (str)
+    """
+
+    def __init__(self, title: str, body: str, created_at: str, last_edited: str):
         if not title.strip():
             raise BlankTitleError()
         if not body.strip():
             raise BlankBodyError()
 
-        self.title = title
-        self.body = body
-        self._created_at = created_at  # Private
-        self.last_edited = last_edited
+        self.title: str = title
+        self.body: str = body
+        self._created_at: str = created_at  # Private
+        self.last_edited: str = last_edited
 
     @property
     def created_at(self):
+        """Trivial getter for `created_at`."""
         return self._created_at
 
+    @override
     def __repr__(self) -> str:
-        return "Note('{}', '{}', '{}', '{}')".format(
-            self.title, self.body, self.created_at, self.last_edited
+        return dedent(
+            f"""\
+            Note({repr(self.title)}, {repr(self.body)}, \
+                {repr(self.created_at)}, {repr(self.last_edited)})\
+        """
         )
 
-    def to_dict(self) -> dict:
+    def to_dict(self) -> dict[str, str]:
+        """Converts `self` to a `dict`, used for JSON conversions."""
+
         return {
             "title": self.title,
             "body": self.body,
@@ -85,7 +106,12 @@ class Note:
         }  # Allows **kwargs for dict unpacking later
 
     @staticmethod
-    def new_note(title, body) -> "Note":
+    def new(title: str, body: str) -> "Note":
+        """
+        Helper for creating a new note.
+        This auto-fills the `created_at` and `last_edited` fields.
+        """
+
         created_at = last_edited = datetime.now().isoformat()
         return Note(title, body, created_at, last_edited)
 
@@ -94,28 +120,40 @@ class NoteCollection:
     """A list of `Note` objects, uniquely identified by their titles"""
 
     def __init__(self, notes: list[Note]):
-        self.notes = notes
+        self.notes: list[Note] = notes
 
+    @override
     def __repr__(self) -> str:
         notes_repr = [repr(note) for note in self.notes]
         return f"NoteCollection({notes_repr})"
 
     def to_json(self) -> str:
+        """Converts `self` to a JSON-formatted string."""
         notes_dict = [note.to_dict() for note in self.notes]
         return json.dumps({"NoteCollection": notes_dict}, indent=4)
 
     @property
     def all_titles(self) -> tuple[str, ...]:
+        """Returns all notes' titles as a tuple."""
         return tuple(note.title for note in self.notes)
 
-    def find_note_index(self, title) -> int:
+    def find_note_index(self, title: str) -> int:
+        """Finds a note with the specified `title`, returns -1 if not found."""
         try:
             return self.all_titles.index(title)
         except ValueError:
             return -1  # Use this for raising errors
 
     # Consider using kwargs when calling this
-    def edit_note(self, curr_title, new_title, new_body):
+    def edit_note(self, curr_title: str, new_title: str, new_body: str):
+        """
+        Given a `curr_title`,
+
+        - attempt to find note with `curr_title`
+        - if found, replace body with `new_body`
+        - else, raise `NotFoundError`
+        """
+
         idx = self.find_note_index(curr_title)
 
         if curr_title != new_title and new_title in self.all_titles:
@@ -133,12 +171,23 @@ class NoteCollection:
         note.last_edited = datetime.now().isoformat()
 
     def add_note(self, note: Note):
+        """
+        Adds the given `note` if its title isn't
+        taken, otherwise, raises `TitleAlreadyExistsError`.
+        """
+
         if note.title not in self.all_titles:
             self.notes.append(note)
         else:
             raise TitleAlreadyExistsError(note.title)
 
-    def delete_note(self, title):
+    def delete_note(self, title: str):
+        """
+        Finds and deletes the note with the given `title`.
+
+        Raises `NotFoundError` if note can't be found.
+        """
+
         note_idx = self.find_note_index(title)
         if note_idx != -1:
             return self.notes.pop(note_idx)
@@ -146,7 +195,7 @@ class NoteCollection:
         raise NotFoundError(title)
 
     @staticmethod
-    def validate_json(fp: Path, strict=True) -> None:
+    def validate_json(fp: Path, strict: bool = True) -> None:
         """
         Given a Path `fp`:
             Checks if the file exists and has the ".json" suffix
@@ -165,9 +214,9 @@ class NoteCollection:
 
         # Only ran iff strict=True
         with fp.open() as f:
-            data = json.load(f)
+            data = json.load(f)  # pyright: ignore[reportAny]
             req_note_keys = ("title", "body", "created_at", "last_edited")
-            collection = data.get("NoteCollection")
+            collection = data.get("NoteCollection")  # pyright: ignore[reportAny]
 
             if collection is None:
                 raise FormatError(
@@ -175,16 +224,24 @@ class NoteCollection:
                 )
 
             if not isinstance(collection, list):
-                t = type(collection)
+                t = type(  # pyright: ignore[reportUnknownVariableType]
+                    collection  # pyright: ignore[reportAny]
+                )
                 raise FormatError(f"Expected list for NoteCollection, instead got {t}")
 
-            for note in collection:
-                if not all(note.get(key) for key in req_note_keys):
+            # fmt: off
+            for note in collection:  # pyright: ignore[reportUnknownVariableType]
+                if not all(
+                    note.get(key) for key in req_note_keys  # pyright: ignore[reportUnknownMemberType, reportUnknownArgumentType] pylint: disable[line-too-long]
+                ):
                     raise FormatError("Missing or blank Note keys")
+            # fmt: on
         return
 
     @staticmethod
     def write_to_json(collection: "NoteCollection", fp: Path):
+        """Writes `self` as JSON to the given path `fp`."""
+
         # Non-strict - Only checks fp exists and is json
         NoteCollection.validate_json(fp, strict=False)
         with fp.open("w") as f:
@@ -192,6 +249,8 @@ class NoteCollection:
 
     @staticmethod
     def from_json(fp: Path) -> "NoteCollection":
+        """Creates a `NoteCollection` instance from the given JSON from path `fp`."""
+
         NoteCollection.validate_json(fp)
 
         with fp.open() as f:
@@ -199,21 +258,21 @@ class NoteCollection:
                 return NoteCollection([])
 
             f.seek(0)
-            notes_dict = json.load(f)["NoteCollection"]
+            notes_dict = json.load(f)["NoteCollection"]  # pyright: ignore[reportAny]
 
-        notes = [Note(**note) for note in notes_dict]
+        notes = [Note(**note) for note in notes_dict]  # pyright: ignore[reportAny]
         return NoteCollection(notes)
 
 
 if __name__ == "__main__":  # Create test notes
     ans = input("Overwrite save with test cases? (Y/N): ")
     if ans.strip().lower() != "y":
-        exit()
+        sys.exit(0)
 
     new = NoteCollection(
         [
-            Note.new_note("Test Note", "A placeholder body"),
-            Note.new_note("Another One", "You see me?"),
+            Note.new("Test Note", "A placeholder body"),
+            Note.new("Another One", "You see me?"),
         ]
     )
 
@@ -227,8 +286,8 @@ if __name__ == "__main__":  # Create test notes
     new_json = new.to_json()
     print(new_json)
 
-    fp = Path.joinpath(Path(__file__).parent, "data.json")
-    with fp.open("w", encoding="utf-8") as f:
-        f.write(new_json)
+    save = Path.joinpath(Path(__file__).parent, "data.json")
+    with save.open("w", encoding="utf-8") as save_stream:
+        save_stream.write(new_json)
 
-    print(NoteCollection.from_json(fp))
+    print(NoteCollection.from_json(save))
