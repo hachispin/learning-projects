9852dc0a3e282ed4946ae1df2122b4de8fd4d19e
hachispin <ganalonrainier255@gmail.com>
2025-11-07 20:15:17 +0000

    refactored downloading module
    - switched from `Mutex` to `AtomicUsize` for storing file sizes
    - changed downloading loop in `download_chapter()` to use `into_iter()` (instead of `iter()`
    - returned file/batch/chapter sizes are now `usize`

--- rust/crates/rust_mdex_dl/src/api/download.rs
+++ rust/crates/rust_mdex_dl/src/api/download.rs
@@ -10,7 +10,13 @@ use crate::{
     paths::manga_save_dir,
 };
 
-use std::{path::PathBuf, sync::Arc};
+use std::{
+    path::PathBuf,
+    sync::{
+        Arc,
+        atomic::{AtomicUsize, Ordering},
+    },
+};
 
 use bytes::Bytes;
 use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
@@ -22,10 +28,7 @@ use reqwest::{self, Client, Url};
 use sanitise_file_name::sanitise;
 use serde::Deserialize;
 use serde_json;
-use tokio::{
-    sync::{Mutex, Semaphore},
-    time::Instant,
-};
+use tokio::{sync::Semaphore, time::Instant};
 
 /// Stores the response structure of the [GetChapterCdn](`crate::Endpoint::GetChapterCdn`)
 /// endpoint for deserializing.
@@ -273,7 +276,7 @@ impl DownloadClient {
         Ok(())
     }
 
-    /// Downloads and saves a chapter's images concurrently and returns the total size in MiBs.
+    /// Downloads and saves a chapter's images concurrently and returns the total size in bytes.
     ///
     /// This also creates the dirs needed to store these images.
     async fn download_chapter(
@@ -281,13 +284,13 @@ impl DownloadClient {
         download_info: ChapterDownloadInfo,
         parent_manga_title: &str,
         images_cfg: &Images,
-    ) -> Result<f64> {
+    ) -> Result<usize> {
         let images_cfg = images_cfg.clone();
         let images = download_info.cdn.construct_image_urls(images_cfg.quality)?;
         let zero_pad = format!("{}", images.len()).len();
 
         let chapter_uuid_suffix = download_info.chapter.uuid().to_string()[..8].to_string();
-        let chapter_size = Arc::new(Mutex::new(0f64));
+        let chapter_size = Arc::new(AtomicUsize::new(0));
         let chapter_title = &download_info.chapter.formatted_title();
 
         let parent_manga_title_safe = sanitise(parent_manga_title);
@@ -297,7 +300,10 @@ impl DownloadClient {
             .join(parent_manga_title_safe)
             .join(chapter_title_safe);
 
-        std::fs::create_dir_all(&chapter_dir).into_diagnostic()?;
+        tokio::fs::create_dir_all(&chapter_dir)
+            .await
+            .into_diagnostic()?;
+
         let chapter_dir = chapter_dir.canonicalize().into_diagnostic()?;
         let mut handles = Vec::with_capacity(images.len() + 1);
         let handle_client = Arc::new(self.clone());
@@ -312,13 +318,13 @@ impl DownloadClient {
         let pb = Arc::new(download_info.pb);
         let start = Instant::now();
 
-        for (i, url) in images.iter().enumerate() {
-            // clone for async **move**
+        for (i, url) in images.into_iter().enumerate() {
+            let chapter_uuid_suffix = chapter_uuid_suffix.clone();
+            let chapter_dir = chapter_dir.clone();
+
+            // `Arc<T>` clones
             let semaphore = self.image_semaphore.clone();
             let pb = pb.clone();
-            let url = url.clone();
-            let chapter_dir = chapter_dir.clone();
-            let chapter_uuid_suffix = chapter_uuid_suffix.clone();
             let chapter_size = chapter_size.clone();
             let h = handle_client.clone();
 
@@ -326,19 +332,19 @@ impl DownloadClient {
                 let _permit = semaphore.acquire().await.into_diagnostic()?;
                 let page = format!("{:0>zero_pad$}", i);
                 let data = h.download_image(&url).await?;
-                let page_size = data.0.len() as f64 / 1_048_576.0; // MiB conversion
+
+                let size_bytes = data.0.len();
+                let size_mib = size_bytes as f64 / 1_048_576.0;
 
                 debug!(
                     "chapter_uuid_suffix={} page={} dl_time_ms={} size_mib={:.3}",
                     chapter_uuid_suffix,
                     page,
                     (Instant::now() - start).as_millis(),
-                    page_size,
+                    size_mib,
                 );
 
-                let mut total = chapter_size.lock().await;
-                *total += page_size;
-
+                chapter_size.fetch_add(size_bytes, Ordering::Relaxed);
                 h.save_image(data, chapter_dir, &page).await?;
 
                 pb.inc(1);
@@ -350,15 +356,17 @@ impl DownloadClient {
             .await
             .into_diagnostic()?;
 
+        let chapter_size = chapter_size.load(Ordering::Relaxed);
+
         info!(
             "({}) Completed downloads in {}ms, total size is {:.3} MiB",
             chapter_uuid_suffix,
             (Instant::now() - start).as_millis(),
-            *chapter_size.lock().await,
+            chapter_size as f64 / 1_048_576.0,
         );
 
         pb.finish_and_clear();
-        Ok(*chapter_size.lock().await)
+        Ok(chapter_size)
     }
 
     /// Helper for [`Self::download_chapters`]
@@ -368,9 +376,9 @@ impl DownloadClient {
         parent_manga: Arc<Manga>,
         pb_multi: &MultiProgress,
         images_cfg: &Images,
-    ) -> Result<f64> {
+    ) -> Result<usize> {
         let start = Instant::now();
-        let batch_size = Arc::new(Mutex::new(0f64));
+        let batch_size = Arc::new(AtomicUsize::new(0));
         let batch_len = batch.len();
         let parent_uuid = parent_manga.uuid();
         let parent_manga_title = parent_manga.title(self.language);
@@ -389,11 +397,13 @@ impl DownloadClient {
 
             pb_multi.add(info.pb.clone());
 
-            let semaphore = self.chapter_semaphore.clone();
+            let h = self.clone();
             let images_cfg = images_cfg.clone();
             let parent_manga_title = parent_manga_title.clone();
+
+            // arc clones
+            let semaphore = self.chapter_semaphore.clone();
             let batch_size = batch_size.clone();
-            let h = self.clone();
 
             handles.push(tokio::spawn(async move {
                 let _permit = semaphore.acquire().await.into_diagnostic()?;
@@ -402,8 +412,7 @@ impl DownloadClient {
                     .download_chapter(info, &parent_manga_title, &images_cfg)
                     .await?;
 
-                let mut total = batch_size.lock().await;
-                *total += chapter_size;
+                batch_size.fetch_add(chapter_size, Ordering::Relaxed);
 
                 Ok::<(), ErrReport>(())
             }));
@@ -413,14 +422,16 @@ impl DownloadClient {
             .await
             .into_diagnostic()?;
 
+        let batch_size = batch_size.load(Ordering::Relaxed);
+
         info!(
             "Batch download ({} chapters) completed in {}ms, total size is {:.3} MiB",
             batch_len,
             (Instant::now() - start).as_millis(),
-            *batch_size.lock().await,
+            batch_size as f64 / 1_048_576.0,
         );
 
-        Ok(*batch_size.lock().await)
+        Ok(batch_size)
     }
 
     /// Downloads all chapters given.
@@ -437,24 +448,22 @@ impl DownloadClient {
         parent_manga: Manga,
         images_cfg: &Images,
     ) -> Result<()> {
+        let start = Instant::now();
+        let pb_multi = MultiProgress::new();
         let parent_manga = Arc::new(parent_manga);
+        let manga_size = Arc::new(AtomicUsize::new(0));
 
         info!(
-            "Downloading {} chapters of manga {}, manga_uuid={}",
+            "Downloading {} chapters of manga {:?}, manga_uuid={}",
             chapters.len(),
             parent_manga.title(self.language),
             parent_manga.uuid()
         );
 
-        let pb_multi = MultiProgress::new();
-
         let dl_info_futs: Vec<_> = chapters
             .into_iter()
             .map(|c| async move { ChapterDownloadInfo::new(&api, c).await })
             .collect();
-
-        let start = Instant::now();
-        let manga_size = Arc::new(Mutex::new(0f64));
 
         for batch in dl_info_futs
             .into_iter()
@@ -466,8 +475,9 @@ impl DownloadClient {
             let batch = match batch {
                 Ok(v) => v,
                 Err(e) => {
-                    error!("Encountered error {e} while using fetched cdns in `dl_info_results`!");
-                    error!("This chapter will be skipped; expect missing chapters");
+                    error!(
+                        "Encountered error {e} while using fetched cdns in `dl_info_results`! Expect missing chapters"
+                    );
                     continue;
                 }
             };
@@ -476,14 +486,15 @@ impl DownloadClient {
                 ._download_chapters(batch, parent_manga.clone(), &pb_multi, images_cfg)
                 .await?;
 
-            let mut total = manga_size.lock().await;
-            *total += batch_size;
+            manga_size.fetch_add(batch_size, Ordering::Relaxed);
         }
+
+        let manga_size = manga_size.load(Ordering::Relaxed);
 
         info!(
             "All downloads completed in {}ms, total size is {:.3} MiB",
             (Instant::now() - start).as_millis(),
-            *manga_size.lock().await,
+            manga_size as f64 / 1_048_576.0,
         );
 
         Ok(())
