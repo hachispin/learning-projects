0848a734f38364a02c9f11e3b28612c758ec4ff8
hachispin <ganalonrainier255@gmail.com>
2025-12-19 18:16:31 +0000

    add more docs + swagger links

--- rust/crates/rust_mdex_dl/src/api/endpoints.rs
+++ rust/crates/rust_mdex_dl/src/api/endpoints.rs
@@ -5,33 +5,42 @@ use uuid::Uuid;
 
 /// "Endpoints" in this context are urls that aren't
 /// valid until prefixed with a proper base url.
+/// 
+/// You can find the URIs in the [`Endpoint::as_string`] method.
 #[derive(Debug, Clone)]
 pub enum Endpoint {
     /// Takes a chapter's UUID and returns its info.
     ///
     /// ## References
     ///
-    /// - <https://api.mangadex.org/docs/redoc.html#tag/Chapter/operation/get-chapter-id>
+    /// - [Redoc](https://api.mangadex.org/docs/redoc.html#tag/Chapter/operation/get-chapter-id)
+    /// - [Swagger](https://api.mangadex.org/docs/swagger.html#/Chapter/get-chapter-id)
     GetChapter(Uuid),
     /// Takes a chapter's UUID and returns its download (CDN) info.
     ///
     /// ## References
     ///
-    /// - <https://api.mangadex.org/docs/redoc.html#tag/AtHome/operation/get-at-home-server-chapterId>
+    /// - [Redoc](https://api.mangadex.org/docs/redoc.html#tag/AtHome/operation/get-at-home-server-chapterId)
+    /// - [Swagger](https://api.mangadex.org/docs/swagger.html#/AtHome/get-at-home-server-chapterId)
     GetChapterCdn(Uuid),
     /// Takes a manga's UUID and returns its info.
     ///
     /// ## References
     ///
-    /// - <https://api.mangadex.org/docs/redoc.html#tag/Manga/operation/get-manga-id>
+    /// - [Redoc](https://api.mangadex.org/docs/redoc.html#tag/Manga/operation/get-manga-id)
+    /// - [Swagger](https://api.mangadex.org/docs/swagger.html#/Manga/get-manga-id)
     GetManga(Uuid),
     /// Takes a manga's UUID and returns its chapters.
+    /// 
+    /// - [Redoc](https://api.mangadex.org/docs/redoc.html#tag/Manga/operation/get-manga-id-feed)
+    /// - [Swagger](https://api.mangadex.org/docs/swagger.html#/Manga/get-manga-id-feed)
     GetMangaChapters(Uuid, Vec<(String, String)>),
-    /// Takes search parameters and returns a list of manga.
+    /// Takes search parameters (with query string) and returns a list of manga.
     ///
     /// ## References
     ///
-    /// - <https://api.mangadex.org/docs/redoc.html#tag/Manga/operation/get-search-manga>
+    /// - [Redoc](https://api.mangadex.org/docs/redoc.html#tag/Manga/operation/get-search-manga)
+    /// - [Swagger](https://api.mangadex.org/docs/swagger.html#/Manga/get-search-manga)
     SearchManga(Vec<(String, String)>),
 }
 
